4ebaa5f07e7c13267c56ae0cf9f27a711dd46b80
Alkaid <1125097352@qq.com>
2021-07-31 10:54:00 +0800

    21、合并两个有序链表

--- src/leetcode/editor/cn/MergeTwoSortedLists.java
+++ src/leetcode/editor/cn/MergeTwoSortedLists.java
@@ -4,12 +4,14 @@ class MergeTwoSortedLists {
     public static void main(String[] args) {
         Solution solution = new MergeTwoSortedLists().new Solution();
         ListNode a1,a2,l1;
-        a1 = new ListNode(4);
-        a2 = new ListNode(2,a1);
+        a1 = new ListNode(8);
+        a2 = new ListNode(6,a1);
         l1 = new ListNode(1,a2);
 
-        ListNode b1,b2,l2;
-        b1 = new ListNode(4);
+        ListNode b1,b2,l2,b3,b4;
+        b4 = new ListNode(9);
+        b3 = new ListNode(5,b4);
+        b1 = new ListNode(4,b3);
         b2 = new ListNode(3,b1);
         l2 = new ListNode(1,b2);
 
@@ -42,7 +44,7 @@ class MergeTwoSortedLists {
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
         //非递归实现
         ListNode head = new ListNode(0);
         ListNode first = head;
@@ -80,6 +82,32 @@ class Solution {
             res.next = mergeTwoLists1(l1,l2.next);
         }
         return res;
+    }
+    //新建链表非递归实现
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode();
+        ListNode temp = newHead;
+        while (l1!=null && l2!=null){
+            if(l1.val<l2.val){
+                newHead.val=l1.val;
+                l1 = l1.next;
+                newHead.next=new ListNode();
+                newHead = newHead.next;
+            }else {
+                newHead.val=l2.val;
+                l2 = l2.next;
+                newHead.next=new ListNode();
+                newHead = newHead.next;
+            }
+        }
+        //一个链表指针到最后为null，这时旨在还指在另一个链表的最大的值（也是两个链表的最大值）上
+        if(l1 != null){
+            newHead.val = l1.val;
+        }
+        if(l2 != null){
+            newHead.val = l2.val;
+        }
+        return temp;
     }
 }
 
