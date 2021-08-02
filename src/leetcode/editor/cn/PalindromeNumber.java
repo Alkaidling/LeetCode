507b903ee41703e1001f1945c583e4ab08a0b3dc
Alkaid <1125097352@qq.com>
2021-08-02 09:46:08 +0800

    9、回文数

---
+++ src/leetcode/editor/cn/PalindromeNumber.java
@@ -0,0 +1,15 @@
+package leetcode.editor.cn;
+//Java：回文数
+class PalindromeNumber {
+    public static void main(String[] args) {
+        Solution solution = new PalindromeNumber().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isPalindrome(int x) {
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
