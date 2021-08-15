d0c0976c193eb0bc4ed04688c1a9ef97c48e6e80
Alkaid <1125097352@qq.com>
2021-08-15 14:28:07 +0800

    94、二叉树的中序遍历

---
+++ src/leetcode/editor/cn/BinaryTreeInorderTraversal.java
@@ -0,0 +1,116 @@
+package leetcode.editor.cn;
+
+import sun.reflect.generics.tree.Tree;
+
+import javax.annotation.Resource;
+import javax.swing.tree.TreeNode;
+import java.util.*;
+
+//Java：94、二叉树的中序遍历
+class BinaryTreeInorderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeInorderTraversal().new Solution();
+        List<Integer> result = new ArrayList<>();
+        System.out.println(result);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    //Morris 遍历算法
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        TreeNode predecessor = null;
+
+        while (root != null) {
+            if (root.left != null) {
+                // predecessor 节点就是当前 root 节点向左走一步，然后一直向右走至无法走为止
+                predecessor = root.left;
+                while (predecessor.right != null && predecessor.right != root) {
+                    predecessor = predecessor.right;
+                }
+
+                // 让 predecessor 的右指针指向 root，继续遍历左子树
+                if (predecessor.right == null) {
+                    predecessor.right = root;
+                    root = root.left;
+                }
+                // 说明左子树已经访问完了，我们需要断开链接
+                else {
+                    res.add(root.val);
+                    predecessor.right = null;
+                    root = root.right;
+                }
+            }
+            // 如果没有左孩子，则直接访问右孩子
+            else {
+                res.add(root.val);
+                root = root.right;
+            }
+        }
+        return res;
+    }
+
+
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        Deque<TreeNode> stk = new LinkedList<TreeNode>();
+        while (root != null || !stk.isEmpty()) {
+            while (root != null) {
+                stk.push(root);
+                root = root.left;
+            }
+            root = stk.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+    //递归
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> inorderTraversal1(TreeNode root) {
+
+        if (root == null)
+           return new ArrayList<Integer>();
+
+        if(root.left != null)
+            inorderTraversal(root.left);
+
+        result.add(root.val);
+
+        if(root.right != null)
+            inorderTraversal(root.right);
+
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+}
