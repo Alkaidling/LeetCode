f597d13ada06222ef662b414e5b649d5dc394b56
Alkaid <1125097352@qq.com>
2021-09-19 10:14:28 +0800

    650、只有两个键的键盘

---
+++ src/leetcode/editor/cn/TwoKeysKeyboard.java
@@ -0,0 +1,16 @@
+package leetcode.editor.cn;
+//Java：650、只有两个键的键盘
+class TwoKeysKeyboard {
+    public static void main(String[] args) {
+        Solution solution = new TwoKeysKeyboard().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int minSteps(int n) {
+
+        return 0;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
