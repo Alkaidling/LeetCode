7070324b02bedb5fa2e03c2a73ad54c2446b0e67
Alkaid <1125097352@qq.com>
2021-09-24 14:19:03 +0800

    n皇后问题

--- src/leetcode/editor/cn/NQueen.java
+++ src/leetcode/editor/cn/NQueen.java
@@ -32,23 +32,24 @@ class Solution{
     }
 
     private int process(int i, int[] record, int n) {
-        if(i == n) {
+        if(i == n) {  //当所有行放完时，记录为一种情况
             return 1;
         }
         int res = 0;
         for (int j = 0; j < n; j++) {
-            if(isValid(record, i ,j)){
-                record[i] = j;
-                res += process(i + 1, record, n);
+            if(isValid(record, i ,j)){      //i行j列(0到n)是否可以放
+                record[i] = j;          //找到可以放的位置，放到i行j列
+                res += process(i + 1, record, n);      //递归求下一行及后面的情况
             }
         }
-
         return res;
     }
 
+    //第i行j列是否可以放
     private boolean isValid(int[] record, int i, int j) {
         for (int k = 0; k < i; k++) {
-            if(j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)){
+            //共列（i以前的行已经放在过j列）   i以前行和j列共斜线（纵坐标差值绝对值 == 横坐标差值绝对值）
+            if(j == record[k] || Math.abs(record[k] - j) == Math.abs(k - i)){
                 return false;
             }
         }
