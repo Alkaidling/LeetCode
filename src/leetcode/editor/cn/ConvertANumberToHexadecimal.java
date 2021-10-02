babceb5987c92aae777de0494ebe3c26aa6a4c08
Alkaid <1125097352@qq.com>
2021-10-02 11:02:30 +0800

    405、数字转换为十六进制数

---
+++ src/leetcode/editor/cn/ConvertANumberToHexadecimal.java
@@ -0,0 +1,36 @@
+package leetcode.editor.cn;
+//405、数字转换为十六进制数
+class ConvertANumberToHexadecimal {
+    public static void main(String[] args) {
+        Solution solution = new ConvertANumberToHexadecimal().new Solution();
+        String s = solution.toHex(-181956);
+        System.out.println(s);
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        long div;
+        StringBuilder builder = new StringBuilder();
+        long value = (long)(Integer.MAX_VALUE) * 2 + 2;  // 1 << 32
+        long res = 0;
+        if (num < 0) {
+            res = value + num;
+        }else {
+            res = num;
+        }
+        while (res != 0){
+            div = res % 16;
+            res = res / 16;
+            char digit = div < 10 ? (char) ('0' + div) : (char) ('a' + div - 10);  //替代switch-case
+            builder.append(digit);
+        }
+
+        return builder.reverse().toString();
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
