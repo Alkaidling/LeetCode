84cbda0b859eafecc73437fb8a636974840448ac
Alkaid <1125097352@qq.com>
2021-10-02 18:31:29 +0800

    704、二分查找

--- src/leetcode/editor/cn/BinarySearch.java
+++ src/leetcode/editor/cn/BinarySearch.java
@@ -1,30 +1,34 @@
 package leetcode.editor.cn;
+
 //Java：704、二分查找
 class BinarySearch {
     public static void main(String[] args) {
         Solution solution = new BinarySearch().new Solution();
-        //int[] nums = {-1,0,3,5,9,12};
-        int[] nums = {0,1};
-        int search = solution.search(nums, 0);
+        int[] nums = {-1,0,3,5,9,12};
+        //int[] nums = {0, 1};
+        int search = solution.search(nums, 1);
         System.out.println(search);
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = (start + end) / 2;
-            if (target > nums[mid])
-                start = mid + 1;
-            else if(target < nums[mid])
-                end = mid - 1;
-            else
-                return mid;
+    class Solution {
+
+        public int search(int[] nums, int target) {
+            int left = 0;
+            int right = nums.length - 1;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (target > nums[mid])
+                    left = mid + 1;
+                else if (target < nums[mid])
+                    right = mid - 1;
+                else if(target == nums[mid]){
+                    return mid;
+                }
+            }
+            return -1;
         }
-        return -1;
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
