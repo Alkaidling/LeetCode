d9e04c8b89fc38e1efa9d68c1ab463d3110184ec
Alkaid <1125097352@qq.com>
2021-10-09 15:09:35 +0800

    55、跳跃游戏

---
+++ src/leetcode/editor/cn/JumpGame.java
@@ -0,0 +1,30 @@
+package leetcode.editor.cn;
+
+//55、跳跃游戏
+class JumpGame {
+    public static void main(String[] args) {
+        Solution solution = new JumpGame().new Solution();
+        int[] nums = {3,2,1,0,4};
+        boolean b = solution.canJump(nums);
+        System.out.println(b);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        //执行耗时:3 ms,击败了43.49% 的Java用户 内存消耗:39.9 MB,击败了27.63% 的Java用户
+        public boolean canJump(int[] nums) {
+            int rightmost = 0; //rightmost为最远到达的地方
+            int n = nums.length;
+            for (int i = 0; i < n; i++) {
+                if (rightmost < i) {
+                    return false;  //到不了i位置则一定到不了最后
+                }
+                rightmost = Math.max(rightmost, i + nums[i]);
+            }
+            return true;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
