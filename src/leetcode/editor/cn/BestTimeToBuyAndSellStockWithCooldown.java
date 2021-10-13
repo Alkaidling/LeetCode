2b0f2c9e02978f9f94d550f5802cf781c8a66206
Alkaid <1125097352@qq.com>
2021-10-13 17:08:41 +0800

    309、最佳买卖股票时机含冷冻期

--- src/leetcode/editor/cn/BestTimeToBuyAndSellStockWithCooldown.java
+++ src/leetcode/editor/cn/BestTimeToBuyAndSellStockWithCooldown.java
@@ -23,9 +23,9 @@ class BestTimeToBuyAndSellStockWithCooldown {
             int[][] f = new int[n][3];
             f[0][0] = -prices[0];
             for (int i = 1; i < n; ++i) {
-                f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);
-                f[i][1] = f[i - 1][0] + prices[i];
-                f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);
+                f[i][0] = Math.max(f[i - 1][0], f[i - 1][2] - prices[i]);  //max(不持有，买入=前一天不持有-当天价钱)
+                f[i][1] = f[i - 1][0] + prices[i];                         //前一天卖出
+                f[i][2] = Math.max(f[i - 1][1], f[i - 1][2]);              //max(前一天还在冷冻期，前一天也不在冷冻期)
             }
             return Math.max(f[n - 1][1], f[n - 1][2]);
         }
