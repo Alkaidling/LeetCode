c997e6eb1e8371d40db2b50d644770027ce84ec2
Alkaid <1125097352@qq.com>
2021-10-14 14:20:30 +0800

    139、单词拆分

---
+++ src/leetcode/editor/cn/WordBreak.java
@@ -0,0 +1,64 @@
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//139、单词拆分
+class WordBreak {
+    public static void main(String[] args) {
+        Solution solution = new WordBreak().new Solution();
+        String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+        List<String> wordDict = Arrays.asList("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa");
+        boolean b = solution.wordBreak(s, wordDict);
+        System.out.println(b);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        //TODO
+        // dp
+        public boolean wordBreak(String s, List<String> wordDict) {
+            Set<String> wordDictSet = new HashSet(wordDict);
+            boolean[] dp = new boolean[s.length() + 1];
+            dp[0] = true;
+            for (int i = 1; i <= s.length(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if(dp[j] && wordDictSet.contains(s.substring(j,i))){  //dp[i]= dp[j] && check(s[j..i−1])  (dp[j]:0到j合法，check(s[j..i−1]):检查j到i-1是否合法 )
+                        dp[i] = true;
+                        break;
+                    }
+                }
+            }
+            return dp[s.length()];
+        }
+
+
+        //dfs: Time Limit Exceeded
+        public boolean wordBreak1(String s, List<String> wordDict) {
+            return dfs1(s, wordDict, 0);
+        }
+
+        private boolean dfs1(String s, List<String> wordDict, int start) {
+            int n = s.length();
+            if (start == n) {
+                return true;
+            }
+            boolean res = false;
+            for (int i = start + 1; i <= n; i++) {
+                String substr = s.substring(start, i);
+                if (wordDict.contains(substr)) {
+                    res = dfs1(s, wordDict, i);
+                }
+                if (res) {
+                    return true;
+                }
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
