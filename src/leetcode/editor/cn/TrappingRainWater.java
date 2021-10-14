de88cf7dfcd0c19fae500749f7c2e85dfbe42c07
Alkaid <1125097352@qq.com>
2021-10-14 15:11:24 +0800

    42、接雨水

---
+++ src/leetcode/editor/cn/TrappingRainWater.java
@@ -0,0 +1,27 @@
+package leetcode.editor.cn;
+
+//42、接雨水
+class TrappingRainWater {
+    public static void main(String[] args) {
+        Solution solution = new TrappingRainWater().new Solution();
+        int[] height = {5,5,1,7,1,1,5,2,7,6};
+        int trap = solution.trap(height);
+        System.out.println(trap);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+
+        //TODO
+        public int trap(int[] height) {
+            int n = height.length;
+            int res = 0;
+
+
+
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
