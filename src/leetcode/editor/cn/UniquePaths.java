0e3dd4858e2e3bbb4959cf2b6440af33ce4737db
Alkaid <1125097352@qq.com>
2021-10-17 14:53:14 +0800

    62、不同路径

---
+++ src/leetcode/editor/cn/UniquePaths.java
@@ -0,0 +1,32 @@
+package leetcode.editor.cn;
+
+//62、不同路径
+class UniquePaths {
+    public static void main(String[] args) {
+        Solution solution = new UniquePaths().new Solution();
+        int i = solution.uniquePaths(17, 17);
+        System.out.println(i);
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //O(m*n)  O(m*n)
+        public int uniquePaths(int m, int n) {
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < n; i++) {
+                dp[0][i] = 1;
+            }
+            for (int i = 0; i < m; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+            return dp[m-1][n-1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
