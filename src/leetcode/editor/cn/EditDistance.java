373f5d162cd4eb66695de406a3e61b31aaa61664
Alkaid <1125097352@qq.com>
2021-10-19 16:24:05 +0800

    72、编辑距离

---
+++ src/leetcode/editor/cn/EditDistance.java
@@ -0,0 +1,18 @@
+package leetcode.editor.cn;
+
+//72、编辑距离
+class EditDistance {
+    public static void main(String[] args) {
+        Solution solution = new EditDistance().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        //TODO
+        public int minDistance(String word1, String word2) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
