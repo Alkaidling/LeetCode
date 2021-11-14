e7721e0a2b7c85b58b334faa29b894586d379dd3
Alkaid <1125097352@qq.com>
2021-11-14 15:56:06 +0800

    剑指 Offer 09、用两个栈实现队列

--- src/leetcode/editor/cn/YongLiangGeZhanShiXianDuiLieLcof.java
+++ src/leetcode/editor/cn/YongLiangGeZhanShiXianDuiLieLcof.java
@@ -1,6 +1,6 @@
 package leetcode.editor.cn;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
 
 //Java：剑指 Offer 09、用两个栈实现队列
 class YongLiangGeZhanShiXianDuiLieLcof {
@@ -18,32 +18,33 @@ class YongLiangGeZhanShiXianDuiLieLcof {
         int i3 = cQueue.deleteHead();
         System.out.println(i3);
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class CQueue {
-    private Stack<Integer> stack1;
-    private Stack<Integer> stack2;
 
-    public CQueue() {
-        stack1 = new Stack<>();
-        stack2 = new Stack<>();
-    }
-    
-    public void appendTail(int value) {
-        stack1.push(value);
-    }
-    
-    public int deleteHead() {
-        if (stack2.isEmpty() && !stack1.isEmpty()) {
-            while (!stack1.isEmpty()){
-                stack2.push(stack1.pop());
-            }
-            return stack2.pop();
-        }else if(!stack2.isEmpty()){
-            return stack2.pop();
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class CQueue {
+
+        private ArrayDeque<Integer> stack1;
+        private ArrayDeque<Integer> stack2;
+
+        public CQueue() {
+            stack1 = new ArrayDeque<>();
+            stack2 = new ArrayDeque<>();
         }
-        return -1;
+
+        public void appendTail(int value) {
+            //从栈1进
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if(stack2.isEmpty()){
+                while (!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+            //从栈2出
+            return stack2.isEmpty() ? -1 : stack2.pop();
+        }
     }
-}
 
 /**
  * Your CQueue object will be instantiated and called as such:
