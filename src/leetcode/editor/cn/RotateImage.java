ae1f21ffc9ab48a417ea155c0b6ae97138ec4db3
Alkaid <1125097352@qq.com>
2021-12-14 15:43:35 +0800

    48、旋转图像

---
+++ src/leetcode/editor/cn/RotateImage.java
@@ -0,0 +1,26 @@
+package leetcode.editor.cn;
+
+//48、旋转图像
+class RotateImage {
+    public static void main(String[] args) {
+        Solution solution = new RotateImage().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void rotate(int[][] matrix) {
+            int n = matrix.length;
+            for (int i = 0; i < n / 2; i++) {
+                for (int j = i; j < n - 1 - i; j++) {
+                    int t = matrix[i][j];
+                    matrix[i][j] = matrix[n - 1 - j][i];
+                    matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+                    matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                    matrix[j][n - 1 - i] = t;
+                }
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
