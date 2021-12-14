68f66fbcd97b225a1a919fcc4c5aa5bff5df7a29
Alkaid <1125097352@qq.com>
2021-12-14 15:43:20 +0800

    剑指 Offer 14- II、剪绳子 II

--- src/leetcode/editor/cn/JianShengZiIiLcof.java
+++ src/leetcode/editor/cn/JianShengZiIiLcof.java
@@ -5,27 +5,34 @@ class JianShengZiIiLcof {
     public static void main(String[] args) {
         Solution solution = new JianShengZiIiLcof().new Solution();
     }
+
     //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+    class Solution {
+        // n = n1 + n2 + ... + na
+        // res = n1 * n2 * .. * na  求max(res)
+        // (n1 + n2 + ... na) / a >= a√n1 * n2 * ... na
+        //当n1 == n2 == ... == na时，等号成立,即res取得最大值
 
-    public static final int MOD = 1000000007;
+        //n = a*x  长度为x相等的a段
+        //res = x^a
+        //x^a == x^(n/x) == (x^(1/x))^n , n > 2
+        //即求 x^(1/x)的最大值
+        //x = e ≈ 2.7 取得最大值
 
-    public int cuttingRope(int n) {
-        if (n <= 3) {
-            return n-1;
+        public static final int MOD = 1000000007;
+
+        public int cuttingRope(int n) {
+            if (n < 4) {
+                return n - 1;
+            }
+            long res = 1;
+            while (n >= 5) {
+                n -= 3;
+                res = res * 3 % MOD;
+            }
+            return (int) (res * n % MOD);
         }
-
-        long ans = 1L;
-
-        while (n >= 5) {
-            n -= 3;
-            ans = ans * 3 % MOD;
-        }
-        ans = ans * n;
-        return (int) (ans%MOD);
-
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
