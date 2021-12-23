71743e69f6c4e2fe9ee40554958824eb65e49153
Alkaid <1125097352@qq.com>
2021-12-23 15:16:58 +0800

    102、二叉树的层序遍历

---
+++ src/leetcode/editor/cn/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,58 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+//102、二叉树的层序遍历
+class BinaryTreeLevelOrderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeLevelOrderTraversal().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+            List<List<Integer>> lists = new ArrayList<>();
+            if (root == null) {
+                return lists;
+            }
+            Deque<TreeNode> queue = new ArrayDeque<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> list = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode node = queue.poll();
+                    list.add(node.val);
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                }
+                lists.add(list);
+            }
+            return lists;
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
