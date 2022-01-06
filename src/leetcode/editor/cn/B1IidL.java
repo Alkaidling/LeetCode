88de6fa85e9a485f69f3229980dda990f3b58ed2
Alkaid <1125097352@qq.com>
2022-01-06 14:44:15 +0800

    剑指 Offer II 069、山峰数组的顶部

--- src/leetcode/editor/cn/B1IidL.java
+++ src/leetcode/editor/cn/B1IidL.java
@@ -5,16 +5,28 @@ class B1IidL {
     public static void main(String[] args) {
         Solution solution = new B1IidL().new Solution();
         //int[] arr = {1, 3, 5, 6, 10, 6, 5, 4, 2, 1};
-        int[] arr = {1, 3, 10, 2};
+        int[] arr = {1,2,3};
         int index = solution.peakIndexInMountainArray(arr);
         System.out.println(index);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        public int peakIndexInMountainArray(int[] arr) {
+            int left = 0, right = arr.length - 1;
+            while (left < right) {
+                int mid = left + (right - left) / 2;
+                if(mid < arr.length - 1 && arr[mid] < arr[mid + 1]){
+                    left = mid + 1;
+                }else {
+                    right = mid;
+                }
+            }
+            return left;
+        }
 
         //O(n)
-        public int peakIndexInMountainArray(int[] arr) {
+        public int peakIndexInMountainArray0(int[] arr) {
             int n = arr.length;
             for (int i = 1; i < n-1; i++) {
                 if(arr[i] > arr[i-1] && arr[i] > arr[i+1]){
