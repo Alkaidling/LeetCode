784477b6de8527647ee4244bd3248e48638ddfba
Alkaid <1125097352@qq.com>
2022-01-16 14:13:30 +0800

    剑指 Offer II 098、路径的数目

---
+++ src/leetcode/editor/cn/TwoAoeFn.java
@@ -0,0 +1,42 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+//剑指 Offer II 098、路径的数目
+class TwoAoeFn {
+    public static void main(String[] args) {
+        Solution solution = new TwoAoeFn().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int uniquePaths(int m, int n) {
+            int[] dp = new int[n];
+            Arrays.fill(dp,1);
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[j] += dp[j - 1];
+                }
+            }
+            return dp[n - 1];
+        }
+
+        public int uniquePaths0(int m, int n) {
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                dp[i][0] = 1;
+            }
+            for (int i = 0; i < n; i++) {
+                dp[0][i] = 1;
+            }
+            for (int i = 1; i < m; i++) {
+                for (int j = 1; j < n; j++) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+            return dp[m - 1][n - 1];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
