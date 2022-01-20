5a98da3308f78d9a6e9be20d67a0e106a09817fc
Alkaid <1125097352@qq.com>
2022-01-20 11:31:29 +0800

    剑指 Offer II 108、单词演变

---
+++ src/leetcode/editor/cn/Om3reC.java
@@ -0,0 +1,74 @@
+package leetcode.editor.cn;
+
+import java.util.*;
+
+//剑指 Offer II 108、单词演变
+class Om3reC {
+    public static void main(String[] args) {
+        Solution solution = new Om3reC().new Solution();
+        System.out.println(solution.ladderLength("hit", "cog", Arrays.asList("hot", "dot", "dog", "lot", "log", "cog")));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        Map<String, Integer> wordId = new HashMap<>();
+        List<List<Integer>> edge = new ArrayList<>();
+        int nodeNum = 0;
+
+        public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+            for (String word : wordList) {
+                addEdge(word);
+            }
+            addEdge(beginWord);
+            if (!wordId.containsKey(endWord)) {
+                return 0;
+            }
+            int[] dis = new int[nodeNum];
+            Arrays.fill(dis, Integer.MAX_VALUE);
+            int beginId = wordId.get(beginWord), endId = wordId.get(endWord);
+            dis[beginId] = 0;
+
+            Queue<Integer> que = new LinkedList<>();
+            que.offer(beginId);
+            while (!que.isEmpty()) {
+                int x = que.poll();
+                if (x == endId) {
+                    return dis[endId] / 2 + 1;
+                }
+                for (int it : edge.get(x)) {
+                    if (dis[it] == Integer.MAX_VALUE) {
+                        dis[it] = dis[x] + 1;
+                        que.offer(it);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        public void addEdge(String word) {
+            addWord(word);
+            int id1 = wordId.get(word);
+            char[] array = word.toCharArray();
+            int length = array.length;
+            for (int i = 0; i < length; ++i) {
+                char tmp = array[i];
+                array[i] = '*';
+                String newWord = new String(array);
+                addWord(newWord);
+                int id2 = wordId.get(newWord);
+                edge.get(id1).add(id2);
+                edge.get(id2).add(id1);
+                array[i] = tmp;
+            }
+        }
+
+        public void addWord(String word) {
+            if (!wordId.containsKey(word)) {
+                wordId.put(word, nodeNum++);
+                edge.add(new ArrayList<Integer>());
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
